665abec95e9118f30b9ac510933a10937cd0db6b
Adebayo  Micheal <mcgamma04@yahoo.com>
2022-07-15 15:40:43 +0100

    Update  employee by id  endpoint completed

--- src/main/java/com/stringcodeltd/empoyeeApp/controller/EmployeeController.java
+++ src/main/java/com/stringcodeltd/empoyeeApp/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import com.stringcodeltd.empoyeeApp.models.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
 
@@ -28,11 +29,27 @@ public class EmployeeController {
     }
 //get an employee
 @GetMapping("employee/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long employeedId) throws ResourceNotFoundException {
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value= "id") Long employeedId) throws ResourceNotFoundException {
      Employee employee =  repository.findById(employeedId)
              .orElseThrow(()-> new ResourceNotFoundException("Sorry employee with "+employeedId+ " is not found"));
             return ResponseEntity.ok().body(employee);
     }
+
+ //update employee
+ @PutMapping("employee/{id}")
+ public ResponseEntity<Employee> updateEmployee(@PathVariable(value="id")Long employeeId, @RequestBody Employee emp) throws ResourceNotFoundException {
+        Employee employee = repository.findById(employeeId)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee not found"));
+        employee.setFirstname(emp.getFirstname());
+        employee.setLastname(emp.getLastname());
+        employee.setSalary(emp.getSalary());
+        employee.setEmail(emp.getEmail());
+        employee.setGender(emp.getGender());
+        employee.setDepartment(emp.getDepartment());
+        return  ResponseEntity.ok(repository.save(employee));
+ }
+ //delete
+
 
 
 
